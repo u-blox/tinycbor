ae1e1f58d46ae55b75e9505d985e7576bd44a908
Hamza Rizwan <hamza.rizwan@u-blox.com>
2022-08-01 18:59:15 +0500

    Added ARMCC Compiler Support

--- src/compilersupport_p.h
+++ src/compilersupport_p.h
@@ -128,6 +128,32 @@
 #  define cbor_htonl        _byteswap_ulong
 #  define cbor_ntohs        _byteswap_ushort
 #  define cbor_htons        _byteswap_ushort
+#elif defined(__arm__)
+static inline uint16_t bswap16(uint16_t x) {
+	return ((( x  >> 8 ) & 0xffu ) | (( x  & 0xffu ) << 8 ));
+}
+static inline uint32_t bswap32(uint32_t x) {
+	return ((( x & 0xff000000u ) >> 24 ) |
+			(( x & 0x00ff0000u ) >> 8  ) |
+			(( x & 0x0000ff00u ) << 8  ) |
+			(( x & 0x000000ffu ) << 24 ));
+}
+static inline uint64_t bswap64(uint64_t x) {
+	return ((( x & 0xff00000000000000ull ) >> 56 ) |
+			(( x & 0x00ff000000000000ull ) >> 40 ) |
+			(( x & 0x0000ff0000000000ull ) >> 24 ) |
+			(( x & 0x000000ff00000000ull ) >> 8  ) |
+			(( x & 0x00000000ff000000ull ) << 8  ) |
+			(( x & 0x0000000000ff0000ull ) << 24 ) |
+			(( x & 0x000000000000ff00ull ) << 40 ) |
+			(( x & 0x00000000000000ffull ) << 56 ));
+}
+#  define cbor_ntohll       bswap64
+#  define cbor_htonll       bswap64
+#  define cbor_ntohl        bswap32
+#  define cbor_htonl        bswap32
+#  define cbor_ntohs        bswap16
+#  define cbor_htons        bswap16
 #endif
 #ifndef cbor_ntohs
 #  include <arpa/inet.h>
